ad9682af6e3f2fd407bd633fbcf394fab6c76c67
John Cossar <coss0030@algonquinlive.com>
2020-11-25 16:26:33 -0500

    Completed Module4

---
+++ OdeToFood.Web/Controllers/Restaurants3Controller.cs
@@ -0,0 +1,39 @@
+﻿using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class Restaurants3Controller : Controller
+    {
+        private readonly IRestaurantData db;
+
+        public Restaurants3Controller(IRestaurantData db)
+        {
+            this.db = db;
+        }
+
+        // GET: Restautants3
+
+        //Test
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = db.Get(id);
+
+            if(model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+    }
+}
