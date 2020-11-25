6b4c0f5c59b1465d06db6b1ed9c4341b11b08e71
John Cossar <coss0030@algonquinlive.com>
2020-11-25 14:00:23 -0500

    Chapter4 updated 25-Nov-2020

---
+++ OdeToFood.Data/Services/OdeToFoodDbContext.cs
@@ -0,0 +1,23 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        //defines table that can populate Restaurant Objects. It will have an Id property, a Name property and 
+        //a Cuisine property. when the data comes back we can map the data into the class
+
+        //By naming the property Restaurants by convention, we are telling the framework that there is a Table called Restaurants
+        //by using the Restaurant as a type, we are telling the framework the data of that class
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+
+    }
+}
---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,58 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Restaurant restaurant)
+        {
+            //Entity Framework automatically makes our Id property a primary key
+            //all these behaviours of the EF may be customized
+
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return db.Restaurants;
+
+            //"Fancier, Linq Syntax
+        //    return from r in db.Restaurants
+          //         orderby r.Name
+         //          select r;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+
+            //This does not track changes for multiple users:
+            // var r = Get(restaurant.Id);
+            // r.Name = "";
+            //  db.SaveChanges();
+
+            //Using Optimistic Concurrancy (not covered) does track changes:
+
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
